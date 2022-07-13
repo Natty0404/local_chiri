92e566f1e5f55d248f46735a18d2718121da7268
Natty0404 <106375669+Natty0404@users.noreply.github.com>
2022-07-13 12:20:33 +0000

    set environment

--- local_chiri/config/environments/development.rb
+++ local_chiri/config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+config.hosts << "da26ea15f181448bae16197e62baf765.vfs.cloud9.ap-northeast-1.amazonaws.com"
+
 end
